5d6938a70449eb7ab38f73ed62e2e1705c1eb65c
Jose Villa <Jose.Villa@version1.com>
2023-12-08 07:24:30 +0100

    Exercises 7 and 8

--- 07/Program.cs
+++ 07/Program.cs
@@ -1,10 +1,196 @@
-﻿
-string[] lines = File.ReadAllLines("Input.txt");
+﻿string[] lines = File.ReadAllLines("Input.txt");
 
+var hands = new List<Hand>();
 foreach (var line in lines)
 {
-
+	var lineSplit = line.Split(' ');
+	hands.Add(new Hand(lineSplit[0], Convert.ToInt32(lineSplit[1])));
 }
 
-var result = 0;
+foreach (var hand in hands)
+{
+	hand.HandValue = GetHandValue(hand.Cards);
+	var cardOrders = new List<int>();
+	foreach (var card in hand.Cards)
+	{
+		cardOrders.Add(GetCardValue(card.ToString()));
+	}
+	hand.CardOrders = cardOrders;
+}
+
+var handsOrder = hands
+	.OrderBy(h => h.HandValue)
+	.ThenBy(h => h.CardOrders[0])
+	.ThenBy(h => h.CardOrders[1])
+	.ThenBy(h => h.CardOrders[2])
+	.ThenBy(h => h.CardOrders[3])
+	.ThenBy(h => h.CardOrders[4])
+	.ToList();
+
+int order = 1;
+foreach (var hand in handsOrder)
+{
+	hand.Rank = order;
+	order++;
+}
+
+var result = handsOrder.Sum(h => h.Rank * h.Bid);
 Console.WriteLine(result);
+
+
+//Part2
+hands = new List<Hand>();
+foreach (var line in lines)
+{
+	var lineSplit = line.Split(' ');
+	hands.Add(new Hand(lineSplit[0], Convert.ToInt32(lineSplit[1])));
+}
+
+foreach (var hand in hands)
+{
+	hand.HandValue = GetHandValue(hand.Cards, true);
+	var cardOrders = new List<int>();
+	foreach (var card in hand.Cards)
+	{
+		cardOrders.Add(GetCardValue(card.ToString(), true));
+	}
+	hand.CardOrders = cardOrders;
+}
+
+handsOrder = hands
+	.OrderBy(h => h.HandValue)
+	.ThenBy(h => h.CardOrders[0])
+	.ThenBy(h => h.CardOrders[1])
+	.ThenBy(h => h.CardOrders[2])
+	.ThenBy(h => h.CardOrders[3])
+	.ThenBy(h => h.CardOrders[4])
+	.ToList();
+
+order = 1;
+foreach (var hand in handsOrder)
+{
+	hand.Rank = order;
+	order++;
+}
+
+result = handsOrder.Sum(h => h.Rank * h.Bid);
+Console.WriteLine(result);
+
+
+
+int GetCardValue(string card, bool isPart2 = false)
+{
+	switch (card)
+	{
+		case "2":
+			return 2;
+		case "3":
+			return 3;
+		case "4":
+			return 4;
+		case "5":
+			return 5;
+		case "6":
+			return 6;
+		case "7":
+			return 7;
+		case "8":
+			return 8;
+		case "9":
+			return 9;
+		case "T":
+			return 10;
+		case "J":
+			if (isPart2)
+			{
+				return 1;
+			}
+			return 11;
+		case "Q":
+			return 12;
+		case "K":
+			return 13;
+		case "A":
+			return 14;
+		default:
+			return 0;
+	}
+}
+
+int GetHandValue(string cards, bool isPart2 = false)
+{
+	var cardsCount = cards.GroupBy(c => c).ToDictionary(c => c.Key, c => c.Count());
+	var joker = 'J';
+
+	if (isPart2 && cardsCount.ContainsKey(joker))
+	{
+		char target = cardsCount.Keys.First();
+		foreach (var card in cardsCount.Keys)
+		{
+			if (card != joker)
+			{
+				if (cardsCount[card] > cardsCount[target] || target == joker)
+				{
+					target = card;
+				}
+			}
+		}
+
+		if (target != joker && cardsCount.ContainsKey(joker))
+		{
+			cardsCount[target] += cardsCount[joker];
+			cardsCount.Remove(joker);
+		}
+
+		if (cardsCount.ContainsKey(joker) && cardsCount.Count > 1)
+		{
+			cardsCount.Remove(joker);
+		}
+	}
+
+	if (cardsCount.Max(c => c.Value) == 1)
+	{
+		return 1;
+	}
+	if (cardsCount.Max(c => c.Value) == 2)
+	{
+		if (cardsCount.Where(c => c.Value == 2).Count() == 1)
+		{
+			return 2;
+		}
+		return 3;
+	}
+	if (cardsCount.Max(c => c.Value) == 3)
+	{
+		if (cardsCount.Where(c => c.Value == 2).Count() == 0)
+		{
+			return 4;
+		}
+		return 5;
+	}
+	if (cardsCount.Max(c => c.Value) == 4)
+	{
+		return 6;
+	}
+	if (cardsCount.Max(c => c.Value) == 5)
+	{
+		return 7;
+	}
+	return 0;
+}
+
+class Hand
+{
+	public string Cards { get; set; }
+	public int Bid { get; set; }
+	public int Rank { get; set; }
+	public int HandValue { get; set; }
+	public List<int> CardOrders { get; set; }
+
+	public Hand(string cards, int bid)
+	{
+		Cards = cards;
+		Bid = bid;
+		CardOrders = new List<int>();
+	}
+}
--- 08/Program.cs
+++ 08/Program.cs
@@ -1,7 +1,156 @@
 ﻿
 string[] lines = File.ReadAllLines("Input.txt");
 
+var directions = string.Empty;
+var nodes = new List<Node>();
 foreach (var line in lines)
 {
+	if ((line.StartsWith("L") || line.StartsWith("R")) &&
+		!line.Contains("="))
+	{
+		directions = directions + line;
+	}
+	else if (line != "")
+	{
+		var lineSplit = line.Split(" = ");
+		var lineSplitLR = lineSplit[1].Split(", ");
+		nodes.Add(new Node(
+			lineSplit[0],
+			lineSplitLR[0].Substring(1, lineSplitLR[0].Length - 1),
+			lineSplitLR[1].Substring(0, lineSplitLR[1].Length - 1)));
+	}
+}
 
+
+int directionIndex = 0;
+int steps = 0;
+
+var nextNode = nodes.First(n => n.NodeName == "AAA");
+while (nextNode.NodeName != "ZZZ")
+{
+	nextNode = GetNextNode(nextNode);
+	steps++;
+}
+
+Console.WriteLine(steps);
+
+
+//Part2
+directionIndex = 0;
+var steps2 = SolvePart2();
+
+Console.WriteLine(steps2);
+
+
+Node GetNextNode(Node currentNode)
+{
+	var currentDirection = directions[directionIndex];
+
+	if (directionIndex == directions.Length - 1)
+	{
+		directionIndex = 0;
+	}
+	else
+	{
+		directionIndex++;
+	}
+
+	var nextNode = string.Empty;
+
+	if (currentDirection.ToString() == "L")
+	{
+		nextNode = currentNode.NodeLeft;
+	}
+	else
+	{
+		nextNode = currentNode.NodeRight;
+	}
+
+	return nodes.First(n => n.NodeName == nextNode);
+}
+
+
+long SolvePart2()
+{
+	List<Node> currentNodes = nodes.Where(n => n.NodeName.EndsWith("A")).ToList();
+
+	Dictionary<int, long> stepsNode = new Dictionary<int, long>();
+	long stepsCount = 0;
+
+	while (true)
+	{
+		List<Node> nextNodes = new List<Node>();
+		var currentDirection = directions[directionIndex];
+		if (directionIndex == directions.Length - 1)
+		{
+			directionIndex = 0;
+		}
+		else
+		{
+			directionIndex++;
+		}
+
+		for (int i = 0; i < currentNodes.Count; i++)
+		{
+			var nextNode = string.Empty;
+			if (currentDirection.ToString() == "L")
+			{
+				nextNode = currentNodes[i].NodeLeft;
+			}
+			else
+			{
+				nextNode = currentNodes[i].NodeRight;
+			}
+
+			if (nextNode.EndsWith("Z"))
+			{
+				stepsNode[i] = stepsCount + 1;
+
+				if (stepsNode.Count == currentNodes.Count)
+				{
+					return CalculateLeastCommonMultiple(stepsNode.Values.ToArray());
+				}
+			}
+			nextNodes.Add(nodes.First(n => n.NodeName == nextNode));
+		}
+
+		currentNodes = nextNodes;
+		stepsCount += 1;
+	}
+}
+
+static long CalculateLeastCommonMultiple(long[] numbers)
+{
+	long result = 1;
+	foreach (long number in numbers)
+	{
+		result = (number * result) / CalculateGreatestCommonDivisor(number, result);
+	}
+	return result;
+}
+
+static long CalculateGreatestCommonDivisor(long a, long b)
+{
+	while (b != 0)
+	{
+		long temp = b;
+		b = a % b;
+		a = temp;
+	}
+	return a;
+}
+
+
+class Node
+{
+	public string NodeName { get; set; }
+	public string NodeLeft { get; set; }
+	public string NodeRight { get; set; }
+
+	public Node(string nodeName, string nodeLeft, string nodeRight)
+	{
+		NodeName = nodeName;
+		NodeLeft = nodeLeft;
+		NodeRight = nodeRight;
+	}
 }
