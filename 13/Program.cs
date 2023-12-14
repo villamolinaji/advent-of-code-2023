a354b3f7a03ed5cfa5ec1b2676b0fa005d1d9934
Jose Villa <Jose.Villa@version1.com>
2023-12-14 07:02:41 +0100

    Add 13 and 14

--- 13/Program.cs
+++ 13/Program.cs
@@ -1,10 +1,109 @@
-﻿
-string[] lines = File.ReadAllLines("Input.txt");
+﻿string[] lines = File.ReadAllLines("Input.txt");
 
+var patterns = new List<Pattern>();
+var pattern = new Pattern();
+var map = new List<List<char>>();
 foreach (var line in lines)
 {
+	if (line == "")
+	{
+		pattern.Map = map;
+		patterns.Add(pattern);
+		pattern = new Pattern();
+		map = new List<List<char>>();
+	}
+	else
+	{
+		map.Add(line.ToCharArray().ToList());
+	}
+}
+pattern.Map = map;
+patterns.Add(pattern);
 
+foreach (var p in patterns)
+{
+	FindReflection(p, false);
 }
 
-var result = 0;
+var result = patterns.Sum(p => p.Value);
 Console.WriteLine(result);
+
+// Part 2
+foreach (var p in patterns)
+{
+	FindReflection(p, true);
+}
+
+var result2 = patterns.Sum(p => p.Value);
+Console.WriteLine(result2);
+
+
+void FindReflection(Pattern pattern, bool isPart2)
+{
+	int value = 0;
+
+	for (int col = 0; col < pattern.Map[0].Count - 1; col++)
+	{
+		int badCount = 0;
+		for (int i = 0; i < pattern.Map[0].Count; i++)
+		{
+			int left = col - i;
+			int right = col + i + 1;
+			if (left >= 0 &&
+				right < pattern.Map[0].Count)
+			{
+				for (int row = 0; row < pattern.Map.Count; row++)
+				{
+					if (pattern.Map[row][left] != pattern.Map[row][right])
+					{
+						badCount++;
+					}
+				}
+			}
+		}
+		if (badCount == (isPart2 ? 1 : 0))
+		{
+			value += col + 1;
+		}
+	}
+
+	for (int row = 0; row < pattern.Map.Count - 1; row++)
+	{
+		int badCount = 0;
+		for (int i = 0; i < pattern.Map.Count; i++)
+		{
+			int up = row - i;
+			int down = row + i + 1;
+			if (up >= 0 &&
+				down < pattern.Map.Count)
+			{
+				for (int col = 0; col < pattern.Map[0].Count; col++)
+				{
+					if (pattern.Map[up][col] != pattern.Map[down][col])
+					{
+						badCount++;
+					}
+				}
+			}
+		}
+		if (badCount == (isPart2 ? 1 : 0))
+		{
+			value += 100 * (row + 1);
+		}
+	}
+
+	pattern.Value = value;
+}
+
+
+class Pattern
+{
+	public List<List<char>> Map { get; set; }
+	public int Value { get; set; }
+
+	public Pattern()
+	{
+		Map = new List<List<char>>();
+		Value = 0;
+	}
+}
--- 14/Program.cs
+++ 14/Program.cs
@@ -1,10 +1,92 @@
 ﻿
 string[] lines = File.ReadAllLines("Input.txt");
 
+var map = new List<List<char>>();
 foreach (var line in lines)
 {
-
+	map.Add(line.ToCharArray().ToList());
 }
 
-var result = 0;
+Roll();
+
+var result = GetResult();
 Console.WriteLine(result);
+
+// Part 2
+map = new List<List<char>>();
+foreach (var line in lines)
+{
+	map.Add(line.ToCharArray().ToList());
+}
+
+Dictionary<string, int> mapDictionary = new Dictionary<string, int>();
+for (int i = 1; i <= 1000000000; i++)
+{
+	for (int j = 0; j < 4; j++)
+	{
+		Roll();
+		Rotate();
+	}
+
+	string mapString = string.Join("", map.Select(row => new string(row.ToArray())));
+	if (mapDictionary.ContainsKey(mapString))
+	{
+		int cycle = i - mapDictionary[mapString];
+		int cyclesToSkip = (1000000000 - i) / cycle;
+		i += cyclesToSkip * cycle;
+	}
+
+	mapDictionary[mapString] = i;
+}
+
+result = GetResult();
+Console.WriteLine(result);
+
+
+void Roll()
+{
+	for (int col = 0; col < map[0].Count; col++)
+	{
+		for (int row = 0; row < map.Count; row++)
+		{
+			if (map[row][col] == 'O')
+			{
+				int i = 0;
+				while (row - (i + 1) >= 0 &&
+					map[row - (i + 1)][col] == '.')
+				{
+					map[row - (i + 1)][col] = 'O';
+					map[row - i][col] = '.';
+					i++;
+				}
+			}
+		}
+	}
+}
+
+int GetResult()
+{
+	var result = 0;
+	int cont = 1;
+	for (int row = map.Count - 1; row >= 0; row--)
+	{
+		result += map[row].Count(c => c == 'O') * cont;
+		cont++;
+	}
+	return result;
+}
+
+void Rotate()
+{
+	var newMap = new List<List<char>>(); ;
+	for (int col = 0; col < map[0].Count; col++)
+	{
+		string line = string.Empty;
+		for (int row = 0; row < map.Count; row++)
+		{
+			line += map[map.Count - 1 - row][col];
+		}
+		newMap.Add(line.ToCharArray().ToList());
+	}
+	map = newMap;
+}
